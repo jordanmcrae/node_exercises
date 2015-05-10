3be2517c02e4508d41b349ffbfc7462a82669780
Jordan <jordanmcrae@hotmail.com>
2015-05-10 17:39:09 -0400

    Completed planetproto 020.js exercise

--- planetproto/020.js
+++ planetproto/020.js
@@ -1,25 +1,30 @@
 // -> Create a machine object
 //    with a property motors = 4
-__
+
+var machine = {
+  motors: 4
+};
 
 // -> Create a robot object
 //    with a property friendly = true
-__
+var robot = {
+  friendly: true
+};
 
 // -> Create a robby object
-__
+var robby = {};
 
 // -> Make machine the prototype of robot
-__
+robot.__proto__ = machine;  // The __proto__ property of Object.prototype is an accessor property (a getter function and a setter function) that exposes the internal [[Prototype]] (either an object or null) of the object through which it is accessed.
 
 // -> Make robot the prototype of robby
-__
+robby.__proto__ = robot;
 
 // -> What is `robby.motors`?
-claim(robby.motors, __);
+claim(robby.motors, 4);
 
 // -> What is `robby.friendly`?
-claim(robby.friendly, __);
+claim(robby.friendly, true);
 
 
 // ------------------------------------------------
